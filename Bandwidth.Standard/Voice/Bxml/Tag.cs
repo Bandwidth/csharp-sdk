b76880ea5e8163f44e2416f87aaa2f303ace1cb2
Mike Hamer <mhamer@bandwidth.com>
2020-12-04 12:13:16 -0500

    Add tag verb

---
+++ Bandwidth.Standard/Voice/Bxml/Tag.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace Bandwidth.Standard.Voice.Bxml
+{
+    /// <summary>
+    ///   The Tag verb is used to set a new tag value without executing a callback.
+    /// </summary>
+    /// <seealso href="https://dev.bandwidth.com/voice/bxml/verbs/tag.html" />
+    public class Tag : IXmlSerializable, IVerb, IAudioProducer
+    {
+        /// <summary>
+        ///   The new tag value. Leading and trailing whitespace is trimmed. Leave blank to clear the tag.
+        /// </summary>
+        public string Value { get; set; }
+
+        XmlSchema IXmlSerializable.GetSchema()
+        {
+            return null;
+        }
+
+        void IXmlSerializable.ReadXml(XmlReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IXmlSerializable.WriteXml(XmlWriter writer)
+        {
+            writer.WriteString(Value);
+        }
+    }
+}
