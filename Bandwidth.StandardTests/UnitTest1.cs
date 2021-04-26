0042fe718f6948463ee9e39c784f557ce2f7f317
Mike Hamer <mhamer@bandwidth.com>
2021-04-26 12:45:13 -0400

    Initial commit of xUnit project template

---
+++ Bandwidth.StandardTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Bandwidth.StandardTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
