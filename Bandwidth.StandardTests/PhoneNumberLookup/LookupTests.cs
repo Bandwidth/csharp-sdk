6539be6664a02137f1741706c2a287490e87e79d
Mike Hamer <mhamer@bandwidth.com>
2021-06-22 10:55:05 -0400

    Add integration test stubs

--- Bandwidth.StandardTests/PhoneNumberLookup/LookupTests.cs
+++ Bandwidth.StandardTests/PhoneNumberLookup/LookupTests.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Bandwidth.Standard;
+using Bandwidth.Standard.Http.Response;
 using Bandwidth.Standard.PhoneNumberLookup.Models;
 using Xunit;
 
@@ -19,7 +22,7 @@ namespace Bandwidth.StandardTests.PhoneNumberLookup
         }
 
         [Fact]
-        public async Task CreateLookupAsync()
+        public async Task CreateTnLookupRequestAsync()
         {
             var accountId = TestConstants.AccountId;
             var number = TestConstants.From;
@@ -33,6 +36,41 @@ namespace Bandwidth.StandardTests.PhoneNumberLookup
 
             Assert.NotEmpty(response.Data.RequestId);
             Assert.Equal("IN_PROGRESS", response.Data.Status);
+        }
+
+        [Fact]
+        public async Task GetTnLookupResultAsync()
+        {
+            var accountId = TestConstants.AccountId;
+            var number = TestConstants.From;
+
+            var request = new AccountsTnlookupRequest
+            {
+                Tns = new List<string> { number }
+            };
+
+            var requestResponse = await _client.PhoneNumberLookup.APIController.CreateTnLookupRequestAsync(accountId, request);
+
+            ApiResponse<AccountsTnlookupResponse1> resultResponse;
+            do {
+                resultResponse = await _client.PhoneNumberLookup.APIController.GetTnLookupResultAsync(accountId, requestResponse.Data.RequestId);
+                
+                // We hit the rate limit very quickly while checking the status.
+                Thread.Sleep(3000);
+            } while (resultResponse.Data.Status == "IN_PROGRESS");
+
+            Assert.NotEmpty(resultResponse.Data.RequestId);
+            Assert.Equal(requestResponse.Data.RequestId, resultResponse.Data.RequestId);
+
+            Assert.Null(resultResponse.Data.FailedTelephoneNumbers);
+            
+
+
+            
+
+            Assert.Equal("US", resultResponse.Data.Result.First().Country);
+
+            Assert.Equal("COMPLETE", requestResponse.Data.Status);
         }
     }
 }
