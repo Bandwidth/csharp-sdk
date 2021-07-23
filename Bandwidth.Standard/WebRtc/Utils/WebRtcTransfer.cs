25b8f8dfe83d018117eeb3a5efe9a2e349a25804
Jacob Mulford <39915377+jmulford-bw@users.noreply.github.com>
2021-07-23 09:38:40 -0400

    DX-2100 webrtc standalone transfer verb (#28)
    * DX-2100 webrtc standalone transfer verb
    
    * Update WebRTC transfer BXML utils and tests
    
    * Update method BXML methods
    
    Co-authored-by: Mike Hamer <mhamer@bandwidth.com>

--- Bandwidth.Standard/WebRtc/Utils/WebRtcTransfer.cs
+++ Bandwidth.Standard/WebRtc/Utils/WebRtcTransfer.cs
@@ -1,17 +1,27 @@
-using System.ComponentModel;
-
 namespace Bandwidth.Standard.WebRtc.Utils
 {
     public static class WebRtcTransfer {
-        public static string generateBxml(string deviceToken) {
-            return WebRtcTransfer.generateBxml(deviceToken, "sip:sipx.webrtc.bandwidth.com:5060");
+        private const string SipUri = "sip:sipx.webrtc.bandwidth.com:5060";
+
+        public static string GenerateBxml(string deviceToken, string voiceCallId, string sipUri = SipUri)
+        {
+            var bxmlVerb = GenerateBxmlVerb(deviceToken, voiceCallId, sipUri);
+            return GenerateBxml(bxmlVerb);
         }
 
-        public static string generateBxml(string deviceToken, string sipUri) {
-            return "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n"
-                + "<Response><Transfer>\n"
-                + "\t<SipUri uui=\"" + deviceToken + ";encoding=jwt\">" + sipUri + "</SipUri>\n"
-                + "</Transfer></Response>\n";
+        public static string GenerateBxmlVerb(string deviceToken, string voiceCallId, string sipUri = SipUri)
+        {
+            if (voiceCallId == null) {
+                return $"<Transfer><SipUri uui=\"{deviceToken};encoding=jwt\">{sipUri}</SipUri></Transfer>";
+            }
+            
+            voiceCallId = voiceCallId.Substring(1).Replace("-", "");
+            return $"<Transfer><SipUri uui=\"{voiceCallId};encoding=base64,{deviceToken};encoding=jwt\">{sipUri}</SipUri></Transfer>";
+        }
+
+        private static string GenerateBxml(string response)
+        {
+            return $"<?xml version=\"1.0\" encoding=\"UTF-8\" ?><Response>{response}</Response>";
         }
     }
 }
