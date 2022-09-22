78ba2b6e301b2d2b31367d6937eea6966d869af3
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-09-22 14:49:23 -0400

    Remove all previous SDK remnants

--- Bandwidth.Standard/Voice/Bxml/IAudioProducer.cs
+++
@@ -1,9 +0,0 @@
-namespace Bandwidth.Standard.Voice.Bxml
-{
-  /// <summary>
-  ///   Should be implemented by SpeakSentence & PlayMedia
-  /// </summary>
-  public interface IAudioProducer
-  {
-  }
-}
