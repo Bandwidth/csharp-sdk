06e4d85b8e1963ee36d3667cf76ead0506977569
Julia Januchowski <jjanuchowski@bandwidth.com>
2023-10-23 15:55:15 -0400

    SWI-1695: Updated env vars

--- src/Bandwidth.Standard.Test/Integration/RecordingsIntegrationTests.cs
+++ src/Bandwidth.Standard.Test/Integration/RecordingsIntegrationTests.cs
@@ -109,7 +109,7 @@ namespace Bandwidth.Standard.Test.Integration
             var jsonBody = JsonSerializer.Serialize(new
             {
                 os = Environment.GetEnvironmentVariable("OPERATING_SYSTEM"),
-                language = "csharp" + Environment.GetEnvironmentVariable("CSHARP_VERSION"),
+                language = "csharp" + Environment.GetEnvironmentVariable("DOTNET_VERSION"),
                 type = "CALL"
             });
             var options = new RequestOptions
