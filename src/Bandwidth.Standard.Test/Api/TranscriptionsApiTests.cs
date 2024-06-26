2e8753a390308b11206e19feeae64077a7699ead
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2024-06-26 14:18:02 -0400

    Update TranscriptionsApiTests.cs

--- src/Bandwidth.Standard.Test/Api/TranscriptionsApiTests.cs
+++ src/Bandwidth.Standard.Test/Api/TranscriptionsApiTests.cs
@@ -19,8 +19,11 @@ using Xunit;
 
 using Bandwidth.Standard.Client;
 using Bandwidth.Standard.Api;
-// uncomment below to import models
-//using Bandwidth.Standard.Model;
+using Moq;
+using System.Net;
+using System.Data.SqlTypes;
+using Bandwidth.Standard.Model;
+using System.Security.Authentication;
 
 namespace Bandwidth.Standard.Test.Api
 {
@@ -34,10 +37,19 @@ namespace Bandwidth.Standard.Test.Api
     public class TranscriptionsApiTests : IDisposable
     {
         private TranscriptionsApi instance;
+        private Mock<ISynchronousClient> mockClient;
+        private Mock<IAsynchronousClient> mockAsynchronousClient;
+        private Configuration fakeConfiguration;
 
         public TranscriptionsApiTests()
         {
-            instance = new TranscriptionsApi();
+            mockClient = new Mock<ISynchronousClient>();
+            mockAsynchronousClient = new Mock<IAsynchronousClient>();
+            fakeConfiguration = new Configuration();
+            fakeConfiguration.BasePath = "https://voice.bandwidth.com/api/v2";
+            fakeConfiguration.Username = "username";
+            fakeConfiguration.Password = "password";
+            instance = new TranscriptionsApi(mockClient.Object, mockAsynchronousClient.Object, fakeConfiguration);
         }
 
         public void Dispose()
@@ -51,8 +63,7 @@ namespace Bandwidth.Standard.Test.Api
         [Fact]
         public void InstanceTest()
         {
-            // TODO uncomment below to test 'IsType' TranscriptionsApi
-            //Assert.IsType<TranscriptionsApi>(instance);
+            Assert.IsType<TranscriptionsApi>(instance);
         }
 
         /// <summary>
@@ -61,11 +72,16 @@ namespace Bandwidth.Standard.Test.Api
         [Fact]
         public void DeleteRealTimeTranscriptionTest()
         {
-            // TODO uncomment below to test the method and replace null with proper value
-            //string accountId = null;
-            //string callId = null;
-            //string transcriptionId = null;
-            //instance.DeleteRealTimeTranscription(accountId, callId, transcriptionId);
+            string accountId = "9900000";
+            string callId = "c-12345";
+            string transcriptionId = "t-12345";
+
+            var apiResponse = new ApiResponse<Object>(HttpStatusCode.NoContent, null);
+            mockClient.Setup(x => x.Delete<Object>("/accounts/{accountId}/calls/{callId}/transcriptions/{transcriptionId}", It.IsAny<RequestOptions>(), fakeConfiguration)).Returns(apiResponse);
+            var response = instance.DeleteRealTimeTranscriptionWithHttpInfo(accountId, callId, transcriptionId);
+
+            Assert.IsType<ApiResponse<Object>>(response);
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
         }
 
         /// <summary>
@@ -74,12 +90,24 @@ namespace Bandwidth.Standard.Test.Api
         [Fact]
         public void GetRealTimeTranscriptionTest()
         {
-            // TODO uncomment below to test the method and replace null with proper value
-            //string accountId = null;
-            //string callId = null;
-            //string transcriptionId = null;
-            //var response = instance.GetRealTimeTranscription(accountId, callId, transcriptionId);
-            //Assert.IsType<CallTranscriptionResponse>(response);
+            string accountId = "9900000";
+            string callId = "c-12345";
+            string transcriptionId = "t-12345";
+
+            CallTranscription.DetectedLanguageEnum detectedLanguage = CallTranscription.DetectedLanguageEnum.EnUS;
+            CallTranscription.TrackEnum track = CallTranscription.TrackEnum.Inbound;
+
+            CallTranscription transcription = new CallTranscription(detectedLanguage: detectedLanguage, track: track, transcript: "abc 123 this is a test", confidence: 0.9);
+
+            List<CallTranscription> tracks = new List<CallTranscription> { transcription };
+
+            CallTranscriptionResponse callTranscriptionResponse = new CallTranscriptionResponse(accountId: accountId, callId: callId, transcriptionId: transcriptionId, tracks: tracks);
+
+            var apiResponse = new ApiResponse<CallTranscriptionResponse>(HttpStatusCode.NoContent, callTranscriptionResponse);
+            mockClient.Setup(x => x.Get<CallTranscriptionResponse>("/accounts/{accountId}/calls/{callId}/transcriptions/{transcriptionId}", It.IsAny<RequestOptions>(), fakeConfiguration)).Returns(apiResponse);
+            var response = instance.GetRealTimeTranscription(accountId, callId, transcriptionId);
+
+            Assert.IsType<CallTranscriptionResponse>(response);
         }
 
         /// <summary>
@@ -88,11 +116,17 @@ namespace Bandwidth.Standard.Test.Api
         [Fact]
         public void ListRealTimeTranscriptionsTest()
         {
-            // TODO uncomment below to test the method and replace null with proper value
-            //string accountId = null;
-            //string callId = null;
-            //var response = instance.ListRealTimeTranscriptions(accountId, callId);
-            //Assert.IsType<List<CallTranscriptionMetadata>>(response);
+            string accountId = "9900000";
+            string callId = "c-12345";
+
+            CallTranscriptionMetadata transcription = new CallTranscriptionMetadata(transcriptionId: "t-12345", transcriptionUrl: "https://voice.bandwidth.com/api/v2/accounts/9900000/calls/c-12345/transcriptions/t-12345");
+            List<CallTranscriptionMetadata> callTranscriptions = new List<CallTranscriptionMetadata> { transcription };
+
+            var apiResponse = new ApiResponse<List<CallTranscriptionMetadata>>(HttpStatusCode.NoContent, callTranscriptions);
+            mockClient.Setup(x => x.Get<List<CallTranscriptionMetadata>>("/accounts/{accountId}/calls/{callId}/transcriptions", It.IsAny<RequestOptions>(), fakeConfiguration)).Returns(apiResponse);
+            var response = instance.ListRealTimeTranscriptions(accountId, callId);
+
+            Assert.IsType<List<CallTranscriptionMetadata>>(response);
         }
     }
 }
