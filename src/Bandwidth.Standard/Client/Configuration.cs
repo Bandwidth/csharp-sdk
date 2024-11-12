5924c1f89722fbc8994e9dce3f7ed199007a24dd
DX-Bandwidth <dx@bandwidth.com>
2024-11-12 19:00:03 +0000

    Generate SDK with OpenAPI Generator Version

--- src/Bandwidth.Standard/Client/Configuration.cs
+++ src/Bandwidth.Standard/Client/Configuration.cs
@@ -34,7 +34,7 @@ namespace Bandwidth.Standard.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.0-dev";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -113,7 +113,7 @@ namespace Bandwidth.Standard.Client
         public Configuration()
         {
             Proxy = null;
-            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/1.0.0/csharp");
+            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/1.0.0-dev/csharp");
             BasePath = "http://localhost";
             DefaultHeaders = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -1004,7 +1004,7 @@ namespace Bandwidth.Standard.Client
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
             report += "    Version of the API: 1.0.0\n";
-            report += "    SDK Package Version: 1.0.0\n";
+            report += "    SDK Package Version: 1.0.0-dev\n";
 
             return report;
         }
