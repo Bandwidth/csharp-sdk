908407cd8d7dcc6a66c807f90d526d4d92d72d64
ckoegel <ckoegel1006@gmail.com>
2025-10-29 15:46:17 -0400

    smoke test

--- src/Bandwidth.Standard.Test/Smoke/PhoneNumberLookupIntegrationTests.cs
+++ src/Bandwidth.Standard.Test/Smoke/PhoneNumberLookupIntegrationTests.cs
@@ -103,9 +103,6 @@ namespace Bandwidth.Standard.Test.Smoke
             Assert.IsType<string>(getData.Results[0].MessagingProvider);
             Assert.Equal("BANDWIDTH", getData.Results[0].VoiceProvider);
             Assert.Equal("USA", getData.Results[0].CountryCodeA3);
-            Assert.IsType<LatestMessageDeliveryStatusEnum>(getData.Results[0].LatestMessageDeliveryStatus);
-            Assert.IsType<DateTime>(getData.Results[0].InitialMessageDeliveryStatusDate);
-            Assert.IsType<DateTime>(getData.Results[0].LatestMessageDeliveryStatusDate);
         }
 
         /// <summary>
@@ -136,9 +133,6 @@ namespace Bandwidth.Standard.Test.Smoke
             Assert.IsType<string>(data.Results[0].MessagingProvider);
             Assert.Equal("BANDWIDTH", data.Results[0].VoiceProvider);
             Assert.Equal("USA", data.Results[0].CountryCodeA3);
-            Assert.IsType<LatestMessageDeliveryStatusEnum>(data.Results[0].LatestMessageDeliveryStatus);
-            Assert.IsType<DateTime>(data.Results[0].InitialMessageDeliveryStatusDate);
-            Assert.IsType<DateTime>(data.Results[0].LatestMessageDeliveryStatusDate);
         }
     }
 }
