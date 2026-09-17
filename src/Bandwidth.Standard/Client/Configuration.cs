67756fda118d91f182505335c358f07c732ef9a7
ckoegel <ckoegel1006@gmail.com>
2026-09-17 15:57:21 -0400

    SWI-12730 Fix Silent Null Swallowing

--- src/Bandwidth.Standard/Client/Configuration.cs
+++ src/Bandwidth.Standard/Client/Configuration.cs
@@ -60,6 +60,11 @@ namespace Bandwidth.Standard.Client
                     string.Format("Error calling {0}: {1}", methodName, response.RawContent),
                     response.RawContent, response.Headers);
             }
+            if (status == 0 || !string.IsNullOrEmpty(response.ErrorText))
+            {
+                return new ApiException(status,
+                    string.Format("Error calling {0}: {1}", methodName, response.ErrorText), response.ErrorText);
+            }
             return null;
         };
 
